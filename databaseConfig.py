4149ea493b9ff67aad9c8c28c09a7a58af3f7376
Constance <ccitalan@stedwards.edu>
2021-10-19 13:28:02 -0500

    Another attempt at installing mysql.connector correctly

--- databaseConfig.py
+++ databaseConfig.py
@@ -1,11 +1,11 @@
 import mysql.connector
 
 try:
-    db = mysql.connector.connect(user='root',
+    connection = mysql.connector.connect(user='root',
                                  password='rootPassword',
                                  host='localhost',
                                  database='test_database')
 except Exception as e:
     raise Exception("NO CONNECTION TO THE DATABASE", e)
 
-db.close()
+connection.close()
