7e7fc2e0cdfd129da917709c6ad310dbca597244
Constance <ccitalan@stedwards.edu>
2021-10-29 11:55:12 -0500

    new changed to database config

--- databaseConfig.py
+++ databaseConfig.py
@@ -1,17 +1,15 @@
 import mysql.connector
 
+# def connect_to_database():
+try:
+    connection = mysql.connector.connect(user='root',
+                                         password='rootPassword',
+                                         host='localhost',
+                                         database='test_database')
+    print("Connected")
+except Exception as e:
+    raise Exception("NO CONNECTION TO THE DATABASE", e)
 
-def connect_to_database():
-    try:
-        connection = mysql.connector.connect(user='root',
-                                             password='rootPassword',
-                                             host='localhost',
-                                             database='test_database')
-    except Exception as e:
-        raise Exception("NO CONNECTION TO THE DATABASE", e)
-    return connection
-
-
-def close_connection(self):
-    self.connection.close()
-    print("MySQL connection is closed")
+# def close_connection(self):
+connection.close()
+print("MySQL connection is closed")
